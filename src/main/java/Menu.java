4f9819203ad6fe4c074669fda472533a567b8796
flastare22 <45687091+flastare22@users.noreply.github.com>
2019-02-24 09:53:08 +0100

    Update Menu.java

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -16,7 +16,7 @@ public class Menu extends Panneau implements ActionListener {
 
             super(frame, "");
 
-            JLabel logo=new JLabel(new ImageIcon(".\\src\\main\\java\\com\\fraw\\image\\Le-Helloco-logo.png"));
+            JLabel logo=new JLabel();
             logo.setSize(this.frame.getWidth()/2,this.frame.getHeight()/2);
             logo.setLocation(this.frame.getWidth()/2-this.frame.getWidth()/4,25);
             this.add(logo);
